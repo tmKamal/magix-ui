5c18b13e04cb124fd9ed1e081d656b6a6f115aa5
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-05-28 22:03:13 +0530

    Updates

---
+++ precache-manifest.ef64a107b4c0905ec0079245b5e16bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437ed66c20dda55c4af4ff81a3f49896",
+    "url": "/magix-ui/index.html"
+  },
+  {
+    "revision": "caba7a44833101971034",
+    "url": "/magix-ui/static/css/main.588015e7.chunk.css"
+  },
+  {
+    "revision": "2133075fad8a534d6eb7",
+    "url": "/magix-ui/static/js/2.19efa3dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magix-ui/static/js/2.19efa3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caba7a44833101971034",
+    "url": "/magix-ui/static/js/main.5a548c95.chunk.js"
+  },
+  {
+    "revision": "a970a5569c9ad024748f",
+    "url": "/magix-ui/static/js/runtime-main.79fbb6ac.js"
+  }
+]);
